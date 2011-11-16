0f5cba412760d6b32d8f4fc32708936507c20e41
Marco Behnke <marco@behnke.biz>
2011-11-16 16:19:28 +0100

    add jquery ui class

---
+++ appBase/biz/behnke/jquery/ui/jQueryUI.php
@@ -0,0 +1,17 @@
+<?php
+namespace biz\behnke\jquery\ui;
+use biz\behnke\jquery\jQuery;
+
+/**
+ * Description of jQueryUI
+ *
+ * @author Marco Behnke <marco@behnke.biz>
+ */
+abstract class jQueryUI extends jQuery {
+
+	/**
+	 * jQuery.ui  version for this build
+	 */
+	const VERSION = '1.8.16';
+
+}
