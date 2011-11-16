9626538d277a0546ff0965f434b1dcd411731772
Marco Behnke <marco@behnke.biz>
2011-11-16 16:18:29 +0100

    add jquery config object, that will hold all configuration properties

---
+++ appBase/biz/behnke/jquery/jQueryConfig.php
@@ -0,0 +1,20 @@
+<?php
+namespace biz\behnke\jquery;
+
+/**
+ * Description of jQueryConfig
+ *
+ * @author Marco Behnke <marco@behnke.biz>
+ */
+class jQueryConfig {
+
+	static function getInstance(array $properties = array())
+	{
+		$config = new jQueryConfig();
+		foreach($properties as $name=>$value)
+		{
+			$config->$name = $value;
+		}
+		return $config;
+	}
+}
