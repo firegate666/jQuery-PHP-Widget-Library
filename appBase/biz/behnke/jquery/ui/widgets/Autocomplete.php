7761c15d9afa94118ce083e2e5b1eccd543dddb9
Marco Behnke <marco@behnke.biz>
2011-11-17 10:20:22 +0100

    Autocomplete widget

---
+++ appBase/biz/behnke/jquery/ui/widgets/Autocomplete.php
@@ -0,0 +1,35 @@
+<?php
+namespace biz\behnke\jquery\ui\widgets;
+use biz\behnke\jquery\ui\jQueryUI;
+
+/**
+ * Description of Autocomplete
+ *
+ * @author Marco Behnke <marco@behnke.biz>
+ * @see http://jqueryui.com/demos/autocomplete/
+ */
+class Autocomplete extends jQueryUI {
+
+	const CLASSNAME = 'biz\behnke\jquery\ui\widgets\Autocomplete';
+	const METHOD = 'autocomplete';
+
+	protected $defConfig = array(
+		'disabled' => false,
+		'appendTo' => 'body',
+		'autoFocus' => false,
+		'delay' => 300,
+		'minLength' => 1,
+		'position' => array('my' => 'left top', 'at' => 'left bottom', 'collision' => 'none' ),
+		'source' => array(),
+	);
+
+	/**
+	 * @see biz\behnke\jquery\jQuery#renderUI()
+	 */
+	function renderUI()
+	{
+		print '<input id="'.$this->match.'" />';
+		self::add($this);
+	}
+
+}
