897f5f531b405b5a4662c47d469f6d1c94fd0ec2
Marco Behnke <marco@behnke.biz>
2011-11-18 14:53:14 +0100

    use shortcut helper

--- index.php
+++ index.php
@@ -6,17 +6,10 @@ define('DEBUG', true);
 
 require_once __DIR__.'/appBase/index.php';
 use biz\behnke\jquery\jQuery;
-use biz\behnke\jquery\ui\widgets\DatePicker;
-use biz\behnke\jquery\ui\widgets\Slider;
-use biz\behnke\jquery\ui\widgets\Button;
-use biz\behnke\jquery\ui\widgets\Autocomplete;
-use biz\behnke\jquery\ui\widgets\Accordion;
 
-use biz\behnke\w3c\html\blocklevel\H;
-use biz\behnke\w3c\html\blocklevel\Div;
-use biz\behnke\w3c\html\blocklevel\P;
-
-use biz\behnke\w3c\html\inline\A;
+use biz\behnke\jquery\ui\helper as ui;
+use biz\behnke\w3c\html\blocklevel\helper as html;
+use biz\behnke\w3c\html\inline\helper as htmli;
 
 foreach(\biz\behnke\util\Country::xmlList()->country as $country)
 {
@@ -26,62 +19,70 @@ foreach(\biz\behnke\util\Country::xmlList()->country as $country)
 ?>
 <!DOCTYPE html>
 <html>
+
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<title>jQuery-PHP-Widget-Library - Example Page</title>
+
 		<link id="theme-switch" type="text/css" href="assets/jquery-ui-1.8.16.custom/css/ui-lightness/jquery-ui-1.8.16.custom.css" rel="stylesheet" />
+
 		<script type="text/javascript" src="assets/jquery-ui-1.8.16.custom/js/jquery-1.6.2.min.js"></script>
 		<script type="text/javascript" src="assets/jquery-ui-1.8.16.custom/js/jquery-ui-1.8.16.custom.min.js"></script>
     </head>
+
     <body>
 
 		<h1>Some Widget Examples</h1>
 			
-		<div><label for="startdate">Startdatum:</label> <?php DatePicker::getInstance('startdate')->renderUI(); ?></div>
+		<div><label for="startdate">Startdatum:</label> <?php ui\DatePicker('startdate')->renderUI(); ?></div>
 
-		<div><label for="enddate">Enddatum:</label> <?php DatePicker::getInstance('enddate')->renderUI(); ?></div>
+		<div><label for="enddate">Enddatum:</label> <?php ui\DatePicker('enddate')->renderUI(); ?></div>
 
-		<div>Button: <?php Button::getInstance('button')->label('Buttontext')->renderUI(); ?></div>
+		<div>Button: <?php ui\Button('button')->label('Buttontext')->type(htmli\Button())->renderUI(); ?></div>
 
 		<div>Accordion:
 			<?php
-				Accordion::getInstance('accordion')
-					->append(H::getInstance(3)->append(A::getInstance()->attr('href', '#')->append('Section 1')))
-					->append(Div::getInstance()->append(P::getInstance()->append('Text 1')))
-					->append(H::getInstance(3)->append(A::getInstance()->attr('href', '#')->append('Section 2')))
-					->append(Div::getInstance()->append(P::getInstance()->append('Text 2')))
-					->append(H::getInstance(3)->append(A::getInstance()->attr('href', '#')->append('Section 3')))
-					->append(Div::getInstance()->append(P::getInstance()->append('Text 3')))
+				ui\Accordion('accordion')
+					->append(html\H(3)->append(htmli\A()->attr('href', '#')->append('Section 1')))
+					->append(html\Div()->append(html\P()->append('Text 1')))
+					->append(html\H(3)->append(htmli\A()->attr('href', '#')->append('Section 2')))
+					->append(html\Div()->append(html\P()->append('Text 2')))
+					->append(html\H(3)->append(htmli\A()->attr('href', '#')->append('Section 3')))
+					->append(html\Div()->append(html\P()->append('Text 3')))
 					->event('mouseover')
 					->renderUI();
 			?>
 		</div>
 
-		<div>Slider: <?php Slider::getInstance('slider')->value(50)->animate(true)->renderUI(); ?></div>
+		<div>Slider: <?php ui\Slider('slider')->value(50)->animate(true)->renderUI(); ?></div>
 
-		<div><label for="autocomplete">Choose country:</label> <?php Autocomplete::getInstance('autocomplete')->source($autocompleteExample)->renderUI(); ?></div>
+		<div><label for="autocomplete">Choose country:</label> <?php ui\Autocomplete('autocomplete')->source($autocompleteExample)->renderUI(); ?></div>
 
 		<div style="position: absolute; bottom: 0; right: 0;" class="theme-switch">
 			<label>Choose theme:</label>
-			<?php Button::getInstance('ui-lightness')
+			<?php ui\Button('ui-lightness')
 				->label('ui-lightness')
-				->type(Button::TYPE_LINK)
+				->type(htmli\A())
 				->attr('href', "assets/jquery-ui-1.8.16.custom/css/ui-lightness/jquery-ui-1.8.16.custom.css")
 				->renderUI(); ?> |
-			<?php Button::getInstance('ui-darkness')
+			<?php ui\Button('ui-darkness')
 				->label('ui-darkness')
-				->type(Button::TYPE_LINK)
+				->type(htmli\A())
 				->attr('href', "assets/jquery-ui-1.8.16.custom/css/ui-darkness/jquery-ui-1.8.16.custom.css")
 				->renderUI(); ?>
 		</div>
 
-		<script type="text/javascript">
-			jQuery('#startdate').datepicker();
+		<!-- the only javascript in this document -->
+		<script type="text/javascript"><!--
+
 			jQuery('.theme-switch a').click(function(){
 				jQuery('#theme-switch').attr('href', jQuery(this).attr('href'));
 				return false;
 			});
+
+			// finally all aggregate widgets are rendered to js
 			<?php jQuery::renderJS(); ?>
-		</script>
+
+		//--></script>
 		</body>
 </html>
