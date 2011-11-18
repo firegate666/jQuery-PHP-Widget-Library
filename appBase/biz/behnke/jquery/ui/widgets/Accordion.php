052efecc53c2cc4324d4eb16007ec9d811501487
Marco Behnke <marco@behnke.biz>
2011-11-18 13:50:00 +0100

    add accordion

---
+++ appBase/biz/behnke/jquery/ui/widgets/Accordion.php
@@ -0,0 +1,39 @@
+<?php
+namespace biz\behnke\jquery\ui\widgets;
+use biz\behnke\jquery\ui\jQueryUI;
+
+/**
+ * Description of Accordion
+ *
+ * @author Marco Behnke <marco@behnke.biz>
+ * @see http://jqueryui.com/demos/accordion/
+ */
+class Accordion extends jQueryUI {
+
+	const METHOD = 'accordion';
+
+	protected $defConfig = array(
+		'disabled' => false,
+		'active' => 0,
+		'animated' => 'slide',
+		'autoHeight' => true,
+		'clearStyle' => false,
+		'collapsible' => false,
+		'event' => 'click',
+		'fillSpace' => false,
+		'header' => '> li > :first-child,> :not(li):even',
+		'icons' => array('header' => 'ui-icon-triangle-1-e', 'headerSelected' => 'ui-icon-triangle-1-s'),
+		'navigation' => false,
+		'navigationFilter' => '',
+	);
+
+	/**
+	 * @see biz\behnke\jquery\jQuery#renderUI()
+	 */
+	function renderUI()
+	{
+		print sprintf('<div id="%s">%s</div>', $this->match, $this->renderInnerHtml());
+		self::add($this);
+	}
+
+}
